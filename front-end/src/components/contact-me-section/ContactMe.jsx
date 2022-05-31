e8245032b7767a9d4fffa3d16b201b06611ab3dd
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-05-31 01:51:29 -0600

    minor change

--- front-end/src/components/contact-me-section/ContactMe.jsx
+++ front-end/src/components/contact-me-section/ContactMe.jsx
@@ -8,7 +8,7 @@ const ContactMe = () => {
             <h3>
                 Contactame
             </h3>
-            <p><b>Correo:</b>luisgonzalezaldana84@gmail.com</p>
+            <p><b>Correo:</b> luisgonzalezaldana84@gmail.com</p>
             <p><b>Celular:</b> (+502) 3044-2833</p>
 
         </div>
